0f18a242713eaf5a93f10a4045e9c6439a1d0659
RajdeepSaini <93119711+RajdeepSaini@users.noreply.github.com>
2024-01-19 14:15:15 +0000

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/song-details', (req, res) => {
+    // In a real application, you would retrieve song details from a database or an external API
+    const songDetails = {
+        title: req.body.title,
+        author: req.body.author,
+        thumbnail: req.body.thumbnail,
+    };
+
+    res.json(songDetails);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
